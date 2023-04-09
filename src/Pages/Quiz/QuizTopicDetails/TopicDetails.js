c78ea2d6eccd48ed60926a8596c8876b0ab013e1
mdfaysal <faysal7830@gmail.com>
2023-04-10 01:58:56 +0600

    data maping problem solved

--- src/Pages/Quiz/QuizTopicDetails/TopicDetails.js
+++ src/Pages/Quiz/QuizTopicDetails/TopicDetails.js
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const TopicDetails = () => {
 
-    const {topicId} = useParams();
+    const { topicId } = useParams();
     const [topicDetails, setTopicDetails] = useState([]);
 
     useEffect(() => {
         fetch(`https://openapi.programming-hero.com/api/quiz/${topicId}`)
             .then(response => response.json())
-            .then(data => setTopicDetails(data.data))
-    }, [])
+            .then(data => {
+                console.log('my data',data.data)
+                setTopicDetails(data.data)
+            })
+    }, [topicId]);
+
+
+    console.log(topicDetails.questions)
 
     return (
-        <div className='mt-5'>
-            <h2>this is topic details</h2>
+        <div className='mt-5 pt-5 text-center'>
+            <img className='img-fluid' src={topicDetails.logo} alt="" />
+            <h2>Let's solve this {topicDetails.name} questions</h2>
+
+            <Container>
+                <h1>Quiz Questions</h1>
+                <Row>
+                    {
+                        topicDetails.questions.forEach(topicDetail => {
+                            console.log(topicDetail)
+                        })
+                    }
+                </Row>
+            </Container>
         </div>
     );
 };
